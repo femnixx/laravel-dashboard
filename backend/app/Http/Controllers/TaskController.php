86e4181c1995b6c892a6b284dca1cd1b9c7679bf
Candra-333 <kirana.intrnasional@gmail.com>
2026-04-21 13:08:28 +0000

    Version 1

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -32,14 +32,6 @@ class TaskController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreTaskRequest $request, TaskService $taskService)
@@ -56,14 +48,6 @@ class TaskController extends Controller
      * Display the specified resource.
      */
     public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
     {
         //
     }
